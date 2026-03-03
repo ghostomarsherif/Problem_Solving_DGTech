ee42f996fa15480b8b58670f9382b06c9d994001
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2026-03-03 17:55:51 +0200

    Implement basic input/output and total calculation
    Fix input/output operations and calculate total correctly.

---
+++ Phase 1/2 - Input_Output.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int num1;
+    int num2;
+    int num3;
+    cin >>num1>>num2>>num3;
+    int total = num2+num3+num1;
+    cin >>num1>>num2>>num3;
+    cout<<""<<total;  
+    return 0;
+}
