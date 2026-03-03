3eb846430c07e47a7de9c035d18be56f3489c769
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2026-03-03 17:55:20 +0200

    Add Hello World program in C++

---
+++ Phase 1/1 - Hello_world.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    string value_to_print="Hello, World!";
+    cout<<value_to_print;
+    return 0;
+}
