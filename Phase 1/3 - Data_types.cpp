9c07dd4adc06555965d3b1b00c7b52cf863b34f3
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2026-03-03 21:40:06 +0200

    Add C++ program for basic data types
    Implement basic data types and input/output in C++.

---
+++ Phase 1/3 - Data_types.cpp
@@ -0,0 +1,22 @@
+//Problem Link: https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+  int num1;
+  long num2;
+  char c1;
+  float num3;
+  double num4;
+  
+  scanf("%d %ld %c %f %lf",&num1,&num2,&c1,&num3,&num4);
+  printf("%d\n%ld\n%c\n%f\n%lf\n",num1,num2,c1,num3,num4);
+  
+}
